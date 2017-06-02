1b87b1b6dd0e6a067657e2c2e7235bf78c2cd0a3
Sri Harsha Gajavalli <sriharsha.g15@iiits.in>
2017-06-02 10:57:55 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -29,6 +29,20 @@ describe "#display_board in 'lib/display_board.rb" do
 
     end
 
+    it 'prints a board with an O in the center position' do
+      board = [" ", " ", " ", " ", "O", " ", " ", " ", " "]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   | O |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
+
+    end
+
     it 'prints a board with O in the top left' do
       board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
       # Leave hint for assigning the 0 index value of O
